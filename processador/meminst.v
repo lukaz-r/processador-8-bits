67e4ab840975b1644759004f53915ba135e70aea
Lucas Rocha <lucas.rocha3@aluno.ufop.edu.br>
2020-09-21 12:42:55 -0300

    #3 Refatoração

---
+++ processador/meminst.v
@@ -0,0 +1,13 @@
+module meminst(
+    input wire[1:0] leit_rs,
+    input wire[1:0] leit_rt,
+    input wire[1:0] escr_rs,
+    input wire[7:0] escr_dado
+
+    output reg[7:0] dado_rs,
+    output reg[7:0] dado_rt
+);
+
+begin
+    
+end
