104241de1ee6730acb3ab225b864fe742c62c98a
rodrigomspro <71416052+rodrigomspro@users.noreply.github.com>
2020-09-29 14:28:07 -0300

    Update memorydata_tb.v
    adição de mais testes e verificações dos valores das entradas e saídas.

--- test/memorydata_tb.v
+++ test/memorydata_tb.v
@@ -10,101 +10,273 @@ module memorydata_tb ();
     
     reg [7:0] mem[7:0];
 
+    memorydata md(.Rm(Rm), .Wm(Wm), .address(address), .RegVal(RegVal),.Data_out(Data_out));
+
     initial begin
         $dumpfile("memorydata_tb.vcd");
         $dumpvars(0,memorydata_tb);
         
-        #10 // passou 10
+        #5 // Escrita em 00
+           // Tempo 05
         address=8'b00000000;
-        RegVal =8'b00000001;
+        RegVal =8'b00001111;
         Wm=1;
         Rm=0;
-
-        #10 // passou 20
-        address=8'b00000000;
-        Rm=1;
-        Wm=0;
-
-        #10 // passou 30
+        $monitor("Wm = %b,\t Rm = %d, address = %d,\t RegVal= %d,\t Data_out = %d", Wm, Rm, address, RegVal, Data_out);
+        #5 // Escrita em 01
+           // Tempo 10
         address=8'b00000001;
-        RegVal =8'b00000100;
-        Wm=1;
-        Rm=0;
-
-        #10 // passou 40
-        address=8'b00000001;
-        Wm=0;
-        Rm=1;
-
-        #10 // passou 50
-        address=8'b00000011;
-        Wm=0;
-        Rm=1;
-
-        #10 // passou 60
-        address=8'b00000001;
-        Wm=0;
-        Rm=1;
-
-        #10 // passou 70
-        address=8'b00001110;
         RegVal =8'b00001110;
         Wm=1;
         Rm=0;
-
-        #10 // passou 80
-        address=8'b00000011;
-        RegVal =8'b00100011;
+        $monitor("Wm = %b,\t Rm = %d, address = %d,\t RegVal= %d,\t Data_out = %d", Wm, Rm, address, RegVal, Data_out);
+        #5 // Escrita em 02
+           // Tempo 15
+        address=8'b00000010;
+        RegVal =8'b00001101;
         Wm=1;
         Rm=0;
-
-        #05 // passou 85
-        address=8'b00001001;
+        $monitor("Wm = %b,\t Rm = %d, address = %d,\t RegVal= %d,\t Data_out = %d", Wm, Rm, address, RegVal, Data_out);
+        #5 // Escrita  em 03
+           // Tempo 20
+        address=8'b00000011;
+        RegVal =8'b00001100;
+        Wm=1;
+        Rm=0;
+        $monitor("Wm = %b,\t Rm = %d, address = %d,\t RegVal= %d,\t Data_out = %d", Wm, Rm, address, RegVal, Data_out);
+        #5 // Escrita  em 04
+           // Tempo 25
+        address=8'b00000100;
+        RegVal =8'b00001011;
+        Wm=1;
+        Rm=0;
+        $monitor("Wm = %b,\t Rm = %d, address = %d,\t RegVal= %d,\t Data_out = %d", Wm, Rm, address, RegVal, Data_out);
+        #5 // Escrita  em 17
+           // Tempo 30
+        address=8'b00010001;
+        RegVal =8'b00001010;
+        Wm=1;
+        Rm=0;
+        $monitor("Wm = %b,\t Rm = %d, address = %d,\t RegVal= %d,\t Data_out = %d", Wm, Rm, address, RegVal, Data_out);
+        #5 // Escrita  em 06
+           // Tempo 35
+        address=8'b00000110;
+        RegVal =8'b00001001;
+        Wm=1;
+        Rm=0;
+        $monitor("Wm = %b,\t Rm = %d, address = %d,\t RegVal= %d,\t Data_out = %d", Wm, Rm, address, RegVal, Data_out);        
+        #5 // Escrita  em 07
+           // Tempo 40
+        address=8'b00000111;
+        RegVal =8'b00001000;
+        Wm=1;
+        Rm=0;
+        $monitor("Wm = %b,\t Rm = %d, address = %d,\t RegVal= %d,\t Data_out = %d", Wm, Rm, address, RegVal, Data_out);        
+        #5 // Escrita  em 08
+           // Tempo 45
+        address=8'b00001000;
         RegVal =8'b00000111;
         Wm=1;
         Rm=0;
-
-        #10 // passou 95
-        address=8'b00000011;
-        Wm=0;
-        Rm=1;
-
-        #5 // passou 100
-        address=8'b00010001;
-        Wm=0;
-        Rm=1;
-
-        #5 // passou 105
+        $monitor("Wm = %b,\t Rm = %d, address = %d,\t RegVal= %d,\t Data_out = %d", Wm, Rm, address, RegVal, Data_out);        
+        #5 // Escrita  em 09
+           // Tempo 50
         address=8'b00001001;
-        Wm=0;
-        Rm=1;
-
-        #05 // passou 110
-        address=8'b00010001;
-        RegVal =8'b00010001;
+        RegVal =8'b00000110;
         Wm=1;
         Rm=0;
+        $monitor("Wm = %b,\t Rm = %d, address = %d,\t RegVal= %d,\t Data_out = %d", Wm, Rm, address, RegVal, Data_out);        
+        #5 // Escrita  em 10
+           // Tempo 55
+        address=8'b00001010;
+        RegVal =8'b00000101;
+        Wm=1;
+        Rm=0;
+        $monitor("Wm = %b,\t Rm = %d, address = %d,\t RegVal= %d,\t Data_out = %d", Wm, Rm, address, RegVal, Data_out);        
+        #5 // Escrita  em 11
+           // Tempo 60
+        address=8'b00001011;
+        RegVal =8'b00000100;
+        Wm=1;
+        Rm=0;
+        $monitor("Wm = %b,\t Rm = %d, address = %d,\t RegVal= %d,\t Data_out = %d", Wm, Rm, address, RegVal, Data_out);        
+        #5 // Escrita  em 12
+           // Tempo 65
+        address=8'b00001100;
+        RegVal =8'b00000011;
+        Wm=1;
+        Rm=0;
+        $monitor("Wm = %b,\t Rm = %d, address = %d,\t RegVal= %d,\t Data_out = %d", Wm, Rm, address, RegVal, Data_out);        
+        #5 // Escrita em 13
+           // Tempo 70
+        address=8'b00001101;
+        RegVal =8'b00000010;
+        Wm=1;
+        Rm=0;
+        $monitor("Wm = %b,\t Rm = %d, address = %d,\t RegVal= %d,\t Data_out = %d", Wm, Rm, address, RegVal, Data_out);        
+        #5 // Escrita 14
+           // Tempo 75
+        address=8'b00001110;
+        RegVal =8'b00000001;
+        Wm=1;
+        Rm=0;
+        $monitor("Wm = %b,\t Rm = %d, address = %d,\t RegVal= %d,\t Data_out = %d", Wm, Rm, address, RegVal, Data_out);        
+        #5 // Escrita 15
+           // Tempo 80
+        address=8'b1111;
+        RegVal =8'b00000000;
+        Wm=1;
+        Rm=0;
+        $monitor("Wm = %b,\t Rm = %d, address = %d,\t RegVal= %d,\t Data_out = %d", Wm, Rm, address, RegVal, Data_out);        
 
-        #5 // passou 115
-        address=8'b00000011;
-        Wm=0;
-        Rm=1;
-
-        #5 // passou 120
+        #5 // Leitura em 0
+           // Tempo 85
         address=8'b00000000;
         Wm=0;
         Rm=1;
-
-        #5 // passou 125
+        $monitor("Wm = %b,\t Rm = %d, address = %d,\t RegVal= %d,\t Data_out = %d", Wm, Rm, address, RegVal, Data_out);        
+        #5 // Leitura  em 01
+           // Tempo 90
         address=8'b00000001;
         Wm=0;
         Rm=1;
-        
-        #10 // passou 130
+        $monitor("Wm = %b,\t Rm = %d, address = %d,\t RegVal= %d,\t Data_out = %d", Wm, Rm, address, RegVal, Data_out);
+        #5 // Leitura  em 02
+           // Tempo 95
+        address=8'b00000010;
+        Wm=0;
+        Rm=1;
+        $monitor("Wm = %b,\t Rm = %d, address = %d,\t RegVal= %d,\t Data_out = %d", Wm, Rm, address, RegVal, Data_out);
+        #5 // Leitura  em 03
+           // Tempo 100
+        address=8'b00000011;
+        Wm=0;
+        Rm=1;
+        $monitor("Wm = %b,\t Rm = %d, address = %d,\t RegVal= %d,\t Data_out = %d", Wm, Rm, address, RegVal, Data_out);
+        #5 // Leitura  em 04
+           // Tempo 105
+        address=8'b00000100;
+        Wm=0;
+        Rm=1;
+        $monitor("Wm = %b,\t Rm = %d, address = %d,\t RegVal= %d,\t Data_out = %d", Wm, Rm, address, RegVal, Data_out);
+        #5 // Leitura  em 05
+           // Tempo 110
+        address=8'b00000101;
+        Wm=0;
+        Rm=1;
+        $monitor("Wm = %b,\t Rm = %d, address = %d,\t RegVal= %d,\t Data_out = %d", Wm, Rm, address, RegVal, Data_out);
+        #5 // Leitura  em 06
+           // Tempo 115
+        address=8'b00000110;
+        Wm=0;
+        Rm=1;
+        $monitor("Wm = %b,\t Rm = %d, address = %d,\t RegVal= %d,\t Data_out = %d", Wm, Rm, address, RegVal, Data_out);
+        #5 // Leitura  em 07
+           // Tempo 120
+        address=8'b00000111;
+        Wm=0;
+        Rm=1;
+        $monitor("Wm = %b,\t Rm = %d, address = %d,\t RegVal= %d,\t Data_out = %d", Wm, Rm, address, RegVal, Data_out);
+        #5 // Leitura  em 08
+           // Tempo 125
+        address=8'b00001000;
+        Wm=0;
+        Rm=1;
+        $monitor("Wm = %b,\t Rm = %d, address = %d,\t RegVal= %d,\t Data_out = %d", Wm, Rm, address, RegVal, Data_out);
+        #5 // Leitura  em 09
+           // Tempo 130
+        address=8'b00001001;
+        Wm=0;
+        Rm=1;
+        $monitor("Wm = %b,\t Rm = %d, address = %d,\t RegVal= %d,\t Data_out = %d", Wm, Rm, address, RegVal, Data_out);
+        #5 // Leitura  em 10
+           // Tempo 135
+        address=8'b00001010;
+        Wm=0;
+        Rm=1;
+        $monitor("Wm = %b,\t Rm = %d, address = %d,\t RegVal= %d,\t Data_out = %d", Wm, Rm, address, RegVal, Data_out);
+        #5 // Leitura  em 11
+           // Tempo 140
+        address=8'b00001011;
+        Wm=0;
+        Rm=1;
+        $monitor("Wm = %b,\t Rm = %d, address = %d,\t RegVal= %d,\t Data_out = %d", Wm, Rm, address, RegVal, Data_out);
+        #5 // Leitura  em 12
+           // Tempo 145
+        address=8'b00001100;
+        Wm=0;
+        Rm=1;
+        $monitor("Wm = %b,\t Rm = %d, address = %d,\t RegVal= %d,\t Data_out = %d", Wm, Rm, address, RegVal, Data_out);
+        #5 // Leitura em 13
+           // Tempo 150
+        address=8'b00001101;
+        Wm=0;
+        Rm=1;
+        $monitor("Wm = %b,\t Rm = %d, address = %d,\t RegVal= %d,\t Data_out = %d", Wm, Rm, address, RegVal, Data_out);
+        #5 // Leitura 14
+           // Tempo 155
+        address=8'b00001110;
+        Wm=0;
+        Rm=1;
+        $monitor("Wm = %b,\t Rm = %d, address = %d,\t RegVal= %d,\t Data_out = %d", Wm, Rm, address, RegVal, Data_out);
+        #5 // Leitura 17
+           // Tempo 160
+        address=8'b10001;      
+        Wm=0;
+        Rm=1;
+        $monitor("Wm = %b,\t Rm = %d, address = %d,\t RegVal= %d,\t Data_out = %d", Wm, Rm, address, RegVal, Data_out);
+        #5 // Leitura 15
+           // Tempo 165
+        address=8'b00001111;      
+        Wm=0;
+        Rm=1;
+        $monitor("Wm = %b,\t Rm = %d, address = %d,\t RegVal= %d,\t Data_out = %d", Wm, Rm, address, RegVal, Data_out);
+        #5 // Escrita em 35
+           // Tempo 170
+        address=8'b00100011;
+        RegVal =8'b00100101;;
+        Wm=1;
+        Rm=0;
+        $monitor("Wm = %b,\t Rm = %d, address = %d,\t RegVal= %d,\t Data_out = %d", Wm, Rm, address, RegVal, Data_out);
+
+        #5 // Leitura em 35
+           // Tempo 175
+        address=8'b00100011;
+        Wm=0;
+        Rm=1;
+        $monitor("Wm = %b,\t Rm = %d, address = %d,\t RegVal= %d,\t Data_out = %d", Wm, Rm, address, RegVal, Data_out);
+        #5 // Leitura em 37
+           // Tempo 180
+        address=8'b00100101;
+        Wm=0;
+        Rm=1;
+        $monitor("Wm = %b,\t Rm = %d, address = %d,\t RegVal= %d,\t Data_out = %d", Wm, Rm, address, RegVal, Data_out);
+        #5 // Leitura em 00
+           // Tempo 185
+        address=8'b00000000;
+        Wm=0;
+        Rm=1;
+        $monitor("Wm = %b,\t Rm = %d, address = %d,\t RegVal= %d,\t Data_out = %d", Wm, Rm, address, RegVal, Data_out);
+
+//*************************************
+//      Linhas de formato de testes   *
+//      Leitura                        
+//      address=8'b;
+//      Wm=0;
+//      Rm=1;
+//
+//      Escrita
+//      address=8'b;
+//      RegVal =8'b;
+//      Wm=1;
+//      Rm=0;
+//      $monitor("Wm = %b, Rm = %d, address = %d, RegVal= %d
+//                Data_out = %d", Wm, Rm, address, RegVal, Data_out);
+//*************************************
+
+        #10 // passou 
         $monitor("End Simulation");
         $finish;
     end
-
-    memorydata md(.Rm(Rm), .Wm(Wm), .address(address), .RegVal(RegVal),.Data_out(Data_out));
 
 endmodule
