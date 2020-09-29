5d6532cbfbc87a09d415f4440cc23cad3759109a
rodrigomspro <71416052+rodrigomspro@users.noreply.github.com>
2020-09-29 14:24:58 -0300

    Update memorydata.v
    Corrigindo erros do código em relação ao array.

--- processador/memorydata.v
+++ processador/memorydata.v
@@ -4,15 +4,16 @@ module memorydata (
     input Rm, Wm;         // Escrita ou Leitura
     input [7:0]address;        // Endereço
     input [7:0]RegVal;    // dado para escrita
-    output reg [7:0] Data_out=8'b00000000; // Resultado da Leitura da memoria
+    output reg [7:0] Data_out; // Resultado da Leitura da memoria
     
-    reg [7:0] mem[7:0];    // Memoria de 8 bits e x posições
+    reg [7:0] mem[35:0];    // Memoria de 8 bits e x posições
+
     always @(*)begin
         if (Rm==1) begin
-            Data_out <= mem[address];
+            Data_out = mem[address];
         end
         else if (Wm==1) begin
-            mem[address] <= RegVal;
+            mem[address] = RegVal;
         end
     end
 endmodule
